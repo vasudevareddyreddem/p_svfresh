7feb12859a7b7bbcb2f0696fe04d04e4bb9f0c7a
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-12-11 16:03:13 +0530

    addig payment  changes

--- application/views/home/paymentstype.php
+++ application/views/home/paymentstype.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<style>
+.razorpay-payment-button{
+	display:none;
+}
+</style>
 <html>
 <head>
   <?php include("header.php"); ?>
@@ -13,16 +18,57 @@
             <div class="modal-body">
               <div class="row">
                 <div class="container">
-                  <form action="<?php echo base_url('paymentstype'); ?>" method="POST">
-                      <input type="radio" name="payment_type" value="1">Cash on delivery
-                      <br>
-                      <input type="radio" name="payment_type" value="2">Swipe on delivery
-                      <br>
-                      <input type="radio" name="payment_type" value="3">Pay Online
-                      <br>
-                      <?php echo form_error('payment_type','<div class="text-danger">', '</div>'); ?>
-                      <button type="submit" class="btn btn-success" name="submit">Submit</button>
-                  </form>
+					<div id="online_amt_1" style="padding-left:50px;">
+					<form action="<?php echo base_url('paymentstype/success'); ?>" method="post" onSubmit="return checkvalidation_payment(this.form);">
+							<input type="radio" id="radio1"  name="payment" onclick="payment_type(this.value);" value="2"><span >Cash On Delivery</span>
+							<br>
+							<input type="radio" id="radio2" name="payment" onclick="payment_type(this.value);"  value="3"><span >Swipe on Delivery</span>
+							<br>
+							<input type="radio" id="radio3" name="payment" onclick="payment_type(this.value);" value="1"><span>Online Payment</span>
+							<br>
+						  <?php echo form_error('payment_type','<div class="text-danger">', '</div>'); ?>
+						  <button type="submit" class="btn btn-success" name="submit">Submit</button>
+					  </form>
+					</div>
+					<div id="online_amt" style="display:none;padding-left:50px;">
+							<form action="<?php echo base_url('paymentstype/success'); ?>" method="post" onSubmit="return checkvalidation(this.form);">
+									<div class="row" >
+									<span id="paymenterrormsg" style="color:red"></span>
+									 <input type="radio" id="radio11"  name="payment" onclick="payment_type(this.value);" value="2"><span >Cash On Delivery</span>
+							<br>
+							<input type="radio" id="radio22" name="payment" onclick="payment_type(this.value);"  value="3"><span >Swipe on Delivery</span>
+							<br>
+							<input type="radio" id="radio33" name="payment" onclick="payment_type(this.value);" value="1"><span>Online Payment</span>
+							<br>
+									 
+										
+										  
+										</div>
+									
+									 
+									<script
+										src="https://checkout.razorpay.com/v1/checkout.js"
+										data-key="<?php echo $details['key']?>"
+										data-amount="<?php echo $details['amount']?>"
+										data-currency="INR"
+										data-name="<?php echo $details['name']?>"
+										data-image="<?php echo $details['image']?>"
+										data-description="<?php echo $details['description']?>"
+										data-prefill.name="<?php echo $details['prefill']['name']?>"
+										data-prefill.email="<?php echo $details['prefill']['email']?>"
+										data-prefill.contact="<?php echo $details['prefill']['contact']?>"
+										data-notes.shopping_order_id="3456"
+										data-order_id="<?php echo $details['order_id']?>"
+										<?php if ($details['display_currency'] !== 'INR') { ?> data-display_amount="<?php echo $details['amount']?>" <?php } ?>
+										<?php if ($details['display_currency'] !== 'INR') { ?> data-display_currency="<?php echo $details['display_currency']?>" <?php } ?>
+									  >
+									  </script>
+									  						  <button type="submit" class="btn btn-success" name="submit">Submit</button>
+
+											
+									
+							</form>
+					
                 </div>
               </div>
             </div>
@@ -36,3 +82,78 @@
 <?php include("footer.php"); ?>
 </body>
 </html>
+<script>
+function payment_type(ids){
+	$('#paymenterrormsg').html('');
+	if(ids==1){
+		$('#online_amt').show();
+		$('#online_amt_1').hide();
+		document.getElementById("radio33").checked = true;
+	}else{
+		$('#online_amt').hide();
+		$('#online_amt_1').show();
+		if(ids==1){
+			document.getElementById("radio1").checked = true;
+			document.getElementById("radio3").checked = false;
+		}else if(ids==3){
+			document.getElementById("radio2").checked = true;
+			document.getElementById("radio3").checked = false;
+		}else{
+			document.getElementById("radio3").checked = false;
+		}
+		
+	}
+}
+function checkvalidation(form){
+if ($("#radio11").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}if ($("#radio22").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}if ($("#radio33").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}if ($("#radio1").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else if ($("#radio2").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else if ($("#radio3").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else{
+	$('#paymenterrormsg').html('Please select a payment mode method');
+	return false;
+}
+}
+function checkvalidation_payment(form){
+
+if ($("#radio1").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}if ($("#radio11").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else if ($("#radio2").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else if ($("#radio22").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else if ($("#radio3").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else if ($("#radio33").prop("checked")) {
+	$('#paymenterrormsg').html('');
+   return true;
+}else{
+	$('#paymenterrormsg').html('Please select a payment mode method');
+	return false;
+}
+
+
+}
+
+</script>
