16cd133a16a14486b4925652e7b0f18050c5cf3a
kyasararamrana <45551096+kyasararamrana@users.noreply.github.com>
2018-12-18 17:08:05 +0530

    Cancel order

--- application/models/Order_Model.php
+++ application/models/Order_Model.php
@@ -28,31 +28,30 @@ class Order_Model extends CI_Model
   {
     $this->db->select('*');
     $this->db->from('order_tab AS o');
-    $this->db->join('order_items_tab AS oi','o.order_id = oi.order_id');
+    $this->db->join('order_items_tab AS oi','o.order_id = oi.order_id','left');
     $this->db->where('o.user_id',$user_id);
     $this->db->order_by('o.updated_date','desc');
     return $this->db->order_by('o.created_date','desc')->get()->result();
-    //return $this->db->get_where($this->table,array('user_id' => $user_id))->result();
   }
   public function cancel_order($order_items_id='')
   {
     $this->db->where('order_items_id',$order_items_id);
-    $this->db->set(array('status'=>'0'));
+    $this->db->set(array('order_status'=>'0'));
     return $this->db->update($this->table_order_items);
   }
-  
+
   public  function get_order_item_details($order_item_id){
-	  
+
     $this->db->select('*')->from('order_items_tab');
     $this->db->where('order_items_id',$order_item_id);
 	 return $this->db->get()->row_array();
   }
-  
+
 	public  function save_item_review($data){
 	  $this->db->insert('rating_list',$data);
 	  return $this->db->insert_id();
 	}
-	
+
 	public  function check_rating_exits($user_id,$o_i_id){
 		$this->db->select('*')->from('rating_list');
 		$this->db->where('order_item_id',$o_i_id);
