89fc0c61cd7fb513e97f51c41cb2298698d4256e
kyasararamrana <45551096+kyasararamrana@users.noreply.github.com>
2018-12-21 14:15:04 +0530

    Export button for milk products

--- application/views/admin/milk_tot_order_list.php
+++ application/views/admin/milk_tot_order_list.php
@@ -112,33 +112,26 @@
     </section>
 </div>
 <script>
-// $(document).ready(function() {
-//         $('#bootstrap-data-table').DataTable({
-//             "order": [
-//                 [6, "desc"]
-//             ]
-//         });
-//     });
+$(document).ready(function() {
+        $('#bootstrap-data-table').DataTable({
+            "order": [
+                [6, "desc"]
+            ]
+        });
+    });
 	</script>
-  <!-- <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script> -->
-  <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.bootstrap4.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.colVis.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.dataTables.min.js'); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/dataTables.bootstrap4.min.js'); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/dataTables.buttons.min.js'); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/buttons.bootstrap4.min.js'); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/jszip.min.js'); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('assets/js/buttons.html5.min.js'); ?>"></script>
 	<script type="text/javascript">
-  $(document).ready(function() {
-    var table = $('#example').DataTable( {
+  $(document).ready(function(){
+    var table = $('#example').DataTable({
         lengthChange: true,
         buttons: ['excel']
-    } );
-
-    table.buttons().container()
-        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
-} );
+    });
+    table.buttons().container().appendTo( '#example_wrapper .col-md-6:eq(0)' );
+	});
 </script>
