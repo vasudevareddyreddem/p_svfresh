e795b3c2d3805dae2600a939734da10a9651f7ed
kasiumamahesh <43772642+kasiumamahesh@users.noreply.github.com>
2018-12-31 18:37:37 +0530

    commit

--- application/controllers/Slider.php
+++ application/controllers/Slider.php
@@ -215,7 +215,7 @@ $this->load->model('Slider_model');
 			if($data['slider']){
 				$sid=$data['slider']->slider_id;
 				$data['pics']=$this->Slider_model->get_slider_pics($sid);
-				if(count($data['pics']>0)){
+				if(count($data['pics'])>0){
 					$data['picstatus']=1;
 				}
 				else{
