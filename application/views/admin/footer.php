236b788ff81819d69e6a169b4da9988c058cd804
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2019-01-10 16:28:54 +0530

    commit

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,29 +1,27 @@
 </div>
-    </div>
-	<script src="<?php echo base_url().'assets/js/popper.js';?>"></script>
- <script src="<?php echo base_url().'assets/js/plugins.js'?>"></script>
-   
+</div>
     
-    <script src="<?php echo base_url().'assets/js/tooltip.js';?>"></script>
-
+    <script src="<?php echo base_url().'assets/js/popper.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/bootstrap.min.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/bootstrapValidator.min.js';?>"></script>
+
+<!--    <script src="<?php echo base_url().'assets/js/plugins.js'?>"></script>-->
+
+    <script src="<?php echo base_url().'assets/js/tooltip.js';?>"></script>
 
     <script src="<?php echo base_url().'assets/js/jquery.nicescroll.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/scroll-up-bar.min.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/sa-functions.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/chart.min.js';?>"></script>
-	<script src="<?php echo base_url().'assets/js/chosen.jquery.min.js';?>"></script>
-    
-   
-   
+    <script src="<?php echo base_url().'assets/js/chosen.jquery.min.js';?>"></script>
+
     <script src="<?php echo base_url().'assets/js/summernote-lite.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/datatables.min.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/dataTables.bootstrap.min.js';?>"></script>
-   
+
     <script src="<?php echo base_url().'assets/js/scripts.js';?>"></script>
     <script src="<?php echo base_url().'assets/js/custom.js';?>"></script>
-	
+
 
 </body>
 
