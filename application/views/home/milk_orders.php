31cfd29a28a596e7602ffcd3c8f5d1f7d91e00f3
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2019-02-21 11:10:21 +0530

    commit

--- application/views/home/milk_orders.php
+++ application/views/home/milk_orders.php
@@ -85,13 +85,53 @@
               <tfoot>
               </tfoot>
             </table>
+              <form class="" action="" method="post">
+                <div class="row">
+                    <div class="col-md-3">
+                        <input type="text" class="form-control" id="datepicker1" name="" placeholder="from date">
+                    </div>
+                    <div class="col-md-3">
+                        <input type="text" class="form-control" id="todatepicker1" name="" placeholder="to date">
+                    </div>
+                    <div class="col-md-6">
+                        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Pay</button>
+                    </div>
+                </div>
+              </form>
               <a href="<?php echo base_url('/home'); ?>" class="button pull-right back-home-btn">Back to Home</a>
           </div>
         </div>
       </div>
     </div>
   </div>
-
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    <form method="" action="">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Payable Amount</h4>
+        </div>
+        <div class="modal-body">
+          
+            <div class="form-group">
+                <input type="text" class="form-control" name="" value="2000.00" placeholder="Amount">
+              </div>
+              <div class="form-group">
+                <input type="file" class="form-control" name="">
+              </div>
+            
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-info">Submit</button>
+        </div>
+      </div>
+      </form>
+    </div>
+  </div>
   <?php include("footer.php"); ?>
   <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap-datepicker.min.js') ?>"></script>
   <script type="text/javascript">
@@ -172,6 +212,11 @@
           format: 'd-m-yyyy'
       });
 	    $('#todatepicker').datepicker({
+          format: 'd-m-yyyy'
+      });$('#datepicker1').datepicker({
+          format: 'd-m-yyyy'
+      });
+	    $('#todatepicker1').datepicker({
           format: 'd-m-yyyy'
       });
     });
