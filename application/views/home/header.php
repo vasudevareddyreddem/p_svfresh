2d2d79be717207c2b8a3accbf4d783b2eb2ccb48
bayapucg <bayapu.edu004@gmail.com>
2019-03-22 18:58:30 +0530

    commit
    cahgnes

--- application/views/home/header.php
+++ application/views/home/header.php
@@ -17,6 +17,16 @@
 
     <title>SV Fresh | <?php echo $pageTitle; ?></title>
 </head>
+<style>
+.dropdown-backdrop {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 0;
+}
+</style>
 <body class="home">
 
 <div id="header" class="header" >
